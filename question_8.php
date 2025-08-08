3407ddefd9729b8a72f6cb91b830ddd166621a88
alexariyan695 <alexariyanxhowdhury695@gmail.com>
2025-08-08 13:00:04 +0600

    question_8

---
+++ question_8.php
@@ -0,0 +1,31 @@
+<?php
+
+
+
+
+
+
+$numbers = [10, 25, 5, 40, 15];
+
+
+$highest_value = max($numbers);
+$min_value=min($numbers);
+
+echo "The maximum value in the array is: " . $highest_value;
+echo "<br/>";
+echo "This is minmum number in the array" .$min_value;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>

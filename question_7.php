c9ccfd40fb6908a8ffb0ac2988a9f14513dbd77f
alexariyan695 <alexariyanxhowdhury695@gmail.com>
2025-08-08 12:59:08 +0600

    Question_7

---
+++ question_7.php
@@ -0,0 +1,31 @@
+<?php
+// Write a program that checks if a student has passed or failed. Passing marks = 33%.
+
+
+$mark=34;
+
+if($mark<33){
+    echo "You feild this mark";
+}else{
+    echo "you passed this mark";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>

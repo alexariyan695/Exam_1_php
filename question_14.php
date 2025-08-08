3bbf146d49d903b71bfaa0945b55e91e0abaafd3
alexariyan695 <alexariyanxhowdhury695@gmail.com>
2025-08-08 13:03:54 +0600

    question_14

---
+++ question_14.php
@@ -0,0 +1,25 @@
+<?php
+//Create a PHP script to print all even numbers between 1 and 100.
+
+for($i=1; $i<100; $i++){
+    if($i%2==0){
+        echo $i."number is even number";
+        echo "<br/>";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>

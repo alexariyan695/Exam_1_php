83295f188e7da8d25e3bb85af92715c161b1d7a3
alexariyan695 <alexariyanxhowdhury695@gmail.com>
2025-08-08 13:04:49 +0600

    question_15

---
+++ question_15.php
@@ -0,0 +1,27 @@
+<?php
+//Write a PHP program to calculate the factorial of a given number.
+
+$n = 5;  
+
+
+$f = 1; 
+
+for ($i=1; $i <= $n; $i++) {  
+  $f = $f * $i;  
+} 
+
+echo "Factorial of $n is $f";  
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>

5e0dac48024e3d8a3dd699e28b35e25b5c3fd01f
alexariyan695 <alexariyanxhowdhury695@gmail.com>
2025-08-08 12:57:32 +0600

    question_4

---
+++ question_4.php
@@ -0,0 +1,29 @@
+<?php
+
+//Write a PHP program to check if a given number is positive, negative, or zero
+
+$a=10;
+
+if($a%2==0){
+    echo $a."is posetive number";
+}elseif($a%2==1){
+    echo $a."is negtive number";
+}else{
+    echo $a. "is zero number";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
